586d2c6ed74ca12db818fd88557bceb2a9902906
Gianluca-V <vespe.gianluca@gmail.com>
2023-09-11 00:32:31 -0300

    fixed bug with local storage

--- Juegos/Vasos/js/main.js
+++ Juegos/Vasos/js/main.js
@@ -15,13 +15,13 @@ function shuffle() {
 
   // Get the state of the cup containers
   const state = Flip.getState(items);
-  
+
   // Do the actual shuffling of the cup containers
   for (let i = items.length; i >= 0; i--) {
     cupsContainer.appendChild(cupsContainer.children[Math.random() * i | 0]);
     playCupMoveSound();
   }
-  
+
   // Animate the change using the Flip library
   Flip.from(state, {
     absolute: true
@@ -42,7 +42,7 @@ function createCups() {
 
   let cupContainerArray = Array.from(cupContainer);
 
-  if(!firstTime){
+  if (!firstTime) {
     // Add motion blur cups to existing cup containers
     const blurCupHTML = '<div class="motion-blur-cup cup" draggable="false"><img src="resources/cup-img.png" alt="cup-img" draggable="false"></div>';
     for (let index = 0; index < cupContainerArray.length; index++) {
@@ -56,20 +56,20 @@ function createCups() {
       repeat: totalIterations,
       repeatDelay: 0.2,
     });
-    
+
     // Remove motion blur cups after shuffling animation is completed
     cupShuffle.eventCallback("onComplete", () => {
       Array.from(document.getElementsByClassName("motion-blur-cup")).forEach(cup => cup.remove());
     });
-    
+
     cupShuffle.call(shuffle); // Call the shuffle function
   }
-  
+
   firstTime = false;
 
   // Add click event listener to each cup container
   cupContainerArray.forEach((element) => {
-    element.addEventListener('click', () => {
+    element.addEventListener('click', async () => {
       if (!selected) {
         let cup = element.querySelector(".cup");
         cup.classList.add("cup-animation");
@@ -81,14 +81,12 @@ function createCups() {
           element.innerHTML += '<div class="ball"><img src="resources/ball-img.png" alt="ball-img" draggable="false"></div>';
           resultDiv.innerHTML += '<img src="resources/win-img.png" alt="" id="result-img">'
           playWinSound();
-          updateUserPoints(betInput.value)
-          localStorage.setItem('user', JSON.stringify(user));
+          await updateUserPoints(betInput.value)
         } //if loose
         else {
-            resultDiv.innerHTML += '<img src="resources/lose-img.png" alt="" id="result-img">'
-            playLoseSound();
-            updateUserPoints(-betInput.value)
-            localStorage.setItem('user', JSON.stringify(user));
+          resultDiv.innerHTML += '<img src="resources/lose-img.png" alt="" id="result-img">'
+          playLoseSound();
+          await updateUserPoints(-betInput.value)
         }
         // Reset the game after 3 seconds
         setTimeout(() => {
@@ -110,31 +108,31 @@ function deleteCups() {
 }
 
 function playWinSound() {
-    const winSound = document.getElementById('winSound');
-    winSound.currentTime = 0; // Reinicia el audio a la posición inicial
-    winSound.play();
-  }
-  
-  function playLoseSound() {
-    const loseSound = document.getElementById('loseSound');
-    loseSound.currentTime = 0;
-    loseSound.play();
-  }
-  
-  function playCupSound() {
-    const cupSound = document.getElementById('cupSound');
-    cupSound.currentTime = 0;
-    cupSound.play();
-  }
+  const winSound = document.getElementById('winSound');
+  winSound.currentTime = 0; // Reinicia el audio a la posición inicial
+  winSound.play();
+}
 
-  function playCupMoveSound() {
-    const cupMoveSound = document.getElementById('cupMoveSound');
-    cupMoveSound.currentTime = 0;
-    cupMoveSound.play();
-  }
+function playLoseSound() {
+  const loseSound = document.getElementById('loseSound');
+  loseSound.currentTime = 0;
+  loseSound.play();
+}
+
+function playCupSound() {
+  const cupSound = document.getElementById('cupSound');
+  cupSound.currentTime = 0;
+  cupSound.play();
+}
+
+function playCupMoveSound() {
+  const cupMoveSound = document.getElementById('cupMoveSound');
+  cupMoveSound.currentTime = 0;
+  cupMoveSound.play();
+}
 
 // Function to update user points using fetch
-function updateUserPoints(pointsToAddOrSubtract) {
+async function updateUserPoints(pointsToAddOrSubtract) {
   const apiUrl = "http://localhost/server/";
   const requestOptions = {
     method: "PUT",
@@ -147,20 +145,25 @@ function updateUserPoints(pointsToAddOrSubtract) {
     }),
   };
 
-  fetch(apiUrl, requestOptions)
-    .then((response) => {
-      if (response.ok) {
-        // Points updated successfully
-        console.log("Points updated successfully");
-      } else {
-        // Handle the error here
-        console.error("Error updating points");
-      }
-    })
-    .catch((error) => {
-      // Handle network or other errors
-      console.error("Error updating points:", error);
-    });
+  try {
+    response = await fetch(apiUrl, requestOptions)
+
+    if (response.ok) {
+      // Points updated successfully
+      console.log("Points updated successfully");
+      const id = user.id;
+      const points = await getUserPoints(id);
+      user = { id, points };
+      localStorage.setItem('user', JSON.stringify(user));
+    } else {
+      // Handle the error here
+      console.error("Error updating points");
+    }
+  }
+  catch (error) {
+    // Handle network or other errors
+    console.error("Error updating points:", error);
+  }
 }
 
 async function getUserPoints(username) {
