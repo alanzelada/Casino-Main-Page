612655500296f6b9a81bc1e1a019f8106d760863
Gianluca-V <vespe.gianluca@gmail.com>
2023-09-13 13:59:45 -0300

    added point system and changed bet system in dice game

--- Juegos/Dados/javas.js
+++ Juegos/Dados/javas.js
@@ -6,6 +6,22 @@ function closeModal() {
   document.getElementById("myModal").style.display = "none";
   document.getElementById("myModal").classList.remove("modal-active");
 }
+// Función para abrir el modal
+function openBet() {
+  document.getElementById("myBetModal").style.display = "flex";
+  document.getElementById("myBetModal").classList.add("modal-active");
+}
+
+// Función para cerrar el modal
+async function closeBet() {
+  if (parseInt(betInput.value) > parseInt(await getUserPoints(user.id))) {
+    alert("No puedes apostar tanto");
+    return;
+  }
+  document.getElementById("myBetModal").style.display = "none";
+  document.getElementById("myBetModal").classList.remove("modal-active");
+}
+
 let dineroDisponibleElement = document.getElementById('dineroDisponible');
 let apuestaElement = document.getElementById('apuesta');
 const usDadoElement = document.getElementById('usDado');
@@ -15,43 +31,86 @@ const botDado2Element = document.getElementById('botDado2');
 const instrucciones = document.getElementById('instrucciones');
 let butn = 1;
 
-let dineroDisponible = 5000; 
-dineroDisponibleElement.textContent = dineroDisponible;
+const betInput = document.querySelector(".bet-input");
+
+let user = JSON.parse(localStorage.getItem('user'));
+
 function tirarDados() {
-  const apuesta = parseInt(apuestaElement.value);
   const usDado = parseInt(usDadoElement.value);
+  if (usDado > 12 || usDado < 2) {
+    alert("Por favor introduzca un número acorde a los dados")
+    return;
+  }
 
   const dado1 = Math.floor(Math.random() * 6) + 1;
   document.querySelector("img.img1").setAttribute("src", "images/dice" + dado1 + ".png");
-  
+
   const dado2 = Math.floor(Math.random() * 6) + 1;
   document.querySelector("img.img2").setAttribute("src", "images/dice" + dado2 + ".png");
 
   const total = dado1 + dado2;
 
   if (total == usDado) {
-    let ganancias = dineroDisponible + apuesta;
-    dineroDisponible = ganancias;
-    dineroDisponibleElement.textContent = dineroDisponible;
+    updateUserPoints(betInput.value)
     resultElement.textContent = '¡Has ganado!';
-    botDado1Element.textContent = `Dado 1: ${dado1}`;
-    botDado2Element.textContent = `Dado 2: ${dado2}`;
-  } else if(apuesta > dineroDisponible){
-    alert("No posees el dinero suficiente para realizar esa apuesta");
   }
-  else if(apuesta < 100){
-    alert("introduzca una cantidad acorde por favor");
-  }
-  else if(usDado > 12 || usDado < 2){
-    alert("Por favor introduzca un número acorde a los dados")
-  }
-  else{
-    let ganancias = dineroDisponible - apuesta;
-    dineroDisponible = ganancias;
-    dineroDisponibleElement.textContent = dineroDisponible;
+  else {
+    updateUserPoints(-betInput.value)
     resultElement.textContent = 'Has perdido.';
-    botDado1Element.textContent = `Dado 1: ${dado1}`;
-    botDado2Element.textContent = `Dado 2: ${dado2}`;
   }
-  
+
+}
+
+async function updateUserPoints(pointsToAddOrSubtract) {
+  const apiUrl = "http://localhost/server/";
+  const requestOptions = {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      username: user.id,
+      points: pointsToAddOrSubtract,
+    }),
+  };
+
+  try {
+    response = await fetch(apiUrl, requestOptions)
+
+    if (response.ok) {
+      // Points updated successfully
+      console.log("Points updated successfully");
+      const id = user.id;
+      const points = await getUserPoints(id);
+      user = { id, points };
+      localStorage.setItem('user', JSON.stringify(user));
+    } else {
+      // Handle the error here
+      console.error("Error updating points");
+    }
+  }
+  catch (error) {
+    // Handle network or other errors
+    console.error("Error updating points:", error);
+  }
+}
+
+async function getUserPoints(username) {
+  const apiUrl = `http://localhost/server/?username=${username}`;
+
+  try {
+    const response = await fetch(apiUrl);
+    if (response.ok) {
+      const data = await response.json();
+      return data.points;
+    } else {
+      // Handle the error here
+      console.error("Error getting user points");
+      return null;
+    }
+  } catch (error) {
+    // Handle network or other errors
+    console.error("Error getting user points:", error);
+    throw error;
+  }
 }
