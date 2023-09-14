990278a02b1899a784320e9b6544e9cf1e8dd911
Mariano <136939689+MareanoSG@users.noreply.github.com>
2023-09-14 03:43:01 -0300

    Update javas.js

--- Juegos/Dados/javas.js
+++ Juegos/Dados/javas.js
@@ -114,3 +114,12 @@ async function getUserPoints(username) {
     throw error;
   }
 }
+actualizarPuntos();
+async function actualizarPuntos() {
+  try {
+    const puntos = await getUserPoints(user.id);
+    document.getElementById("ptstotales__text").textContent = 'Puntos: ' + puntos;
+  } catch (error) {
+    console.error("Error al obtener los puntos:", error);
+  }
+}
