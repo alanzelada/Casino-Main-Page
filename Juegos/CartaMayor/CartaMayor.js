a6a93c59e94b6983dfabfe96f8712164cdf034d2
Mariano <136939689+MareanoSG@users.noreply.github.com>
2023-09-14 03:42:44 -0300

    Update CartaMayor.js

--- Juegos/CartaMayor/CartaMayor.js
+++ Juegos/CartaMayor/CartaMayor.js
@@ -218,3 +218,12 @@ try {
     throw error;
 }
 }
+actualizarPuntos();
+async function actualizarPuntos() {
+  try {
+    const puntos = await getUserPoints(user.id);
+    document.getElementById("ptstotales__text").textContent = 'Puntos: ' + puntos;
+  } catch (error) {
+    console.error("Error al obtener los puntos:", error);
+  }
+}
